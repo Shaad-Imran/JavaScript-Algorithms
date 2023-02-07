ae02aae6a90691970a236a938df460e044271baa
Imran <imranmansuri038@gmail.com>
2023-02-07 20:26:19 +0530

    Binary Search Algorithm Added

---
+++ BinarySearch.js
@@ -0,0 +1,27 @@
+// Problem - Given a sorted array of n elements and a target element t find the index of t in the array. Return -1 if the target element is not found.
+
+// note - Binary Search only works on sorted array
+
+function binarySearch(arr, target) {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+    if (target === arr[middleIndex]) {
+      return middleIndex;
+    }
+
+    if (target < arr[middleIndex]) {
+      rightIndex = middleIndex - 1;
+    } else {
+      leftIndex = middleIndex + 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch([0, 3, 6, 8, 35], 6));
+console.log(binarySearch([0, 3, 6, 8, 35, 55, 77], 55));
+// Big-O = O(logn)
--- LinearSearch.js
+++ LinearSearch.js
@@ -1,6 +1,6 @@
 // Problem - Given an array of n elements and a target element t find the index of t in the array. Return -1 if the target element is not found.
 
-function findIndex(arr, target) {
+function linearSearch(arr, target) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === target) {
       return i;
@@ -9,8 +9,8 @@ function findIndex(arr, target) {
   return -1;
 }
 
-console.log(findIndex([1, 5, 9], 5));
-console.log(findIndex([1, 5, 9], 3));
-console.log(findIndex([1, 5, 9, 18, 27], 27));
-console.log(findIndex([1, 5, 9, 18, 27, 50], 10));
+console.log(linearSearch([1, 5, 9], 5));
+console.log(linearSearch([1, 5, 9], 3));
+console.log(linearSearch([1, 5, 9, 18, 27], 27));
+console.log(linearSearch([1, 5, 9, 18, 27, 50], 10));
 // Big-O = O(n)
