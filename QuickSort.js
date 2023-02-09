b69adc4d7bcbe891fe9ef187127730d436696c49
Imran <imranmansuri038@gmail.com>
2023-02-09 15:43:58 +0530

    Quicksort algorithm added

---
+++ QuickSort.js
@@ -0,0 +1,23 @@
+// Problem - Given an array of integers, sort the array
+
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+const arr = [8, 20, -2, 4, -6];
+console.log(quickSort(arr));
+
+// Big-O = O(n^2) Worst case
+// Avg case -  O(nlgon)
